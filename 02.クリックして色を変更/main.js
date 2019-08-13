d6f88ed43f8f7e7ae191fcd805177b303bd78acd
aocattleya <aocattleya@yahoo.co.jp>
2019-08-13 14:21:11 +0900

    JavaScript

---
+++ 02.クリックして色を変更/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.getElementById("target").addEventListener("click", function() {
+  document.getElementById("target").style.background = "pink";
+  document.getElementById("target").style.borderRadius = "50%"; // 円にする
+});
---
+++ 04.divを増やす/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const target = document.getElementById("target");
+
+target.addEventListener("click", function() {
+  target.classList.toggle("circle");
+});
