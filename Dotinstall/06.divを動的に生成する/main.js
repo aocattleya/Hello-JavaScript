3d666d9f1d69884047474da982a4127f08fed5b4
aocattleya <aocattleya@yahoo.co.jp>
2019-12-28 19:52:13 +0900

    remove

--- Dotinstall/06.divを動的に生成する/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const div = document.createElement("div");
-div.classList.add("box");
-div.addEventListener("click", function() {
-  div.classList.toggle("circle");
-});
-
-document.body.appendChild(div);
