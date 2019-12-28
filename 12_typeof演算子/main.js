aeec58d8c7061a6b5de47062a8a6ac24f3ee1f39
aocattleya <aocattleya@yahoo.co.jp>
2019-12-29 06:13:19 +0900

    JavaScript

---
+++ 12_typeof演算子/main.js
@@ -0,0 +1,14 @@
+let num = 1;
+console.log(typeof num); // number
+
+let str = "こんにちは";
+console.log(typeof str); // string
+
+let flag = true;
+console.log(typeof flag); // boolean
+
+let ary = ["JavaScript", "Ajax", "ASP.NET"];
+console.log(typeof ary); // object
+
+let obj = { x: 1, y: 2 };
+console.log(typeof obj); // object
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Hello_JavaScript
+## 識別子の記法
+
+変数/関数　 camelCase
+
+定数　アンダースコア
+
+クラス名　 Pascal
